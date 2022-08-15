ee84d2a7b59a3f39dfacb78f1ac51a3916d5bc41
Milton Araújo <miltonjr.dev@gmail.com>
2022-08-15 18:37:00 -0300

    Improvement: public images

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -55,7 +55,7 @@ export const Navbar = () => {
               </Link>
             </li>
             <li>
-              <Link href="/services" passHref>
+              <Link href="/services">
                 <a className="block py-2 pr-4 pl-3 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-rose md:p-0 dark:text-gray-400 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">
                   Serviços
                 </a>
