cdbaa356e7cc0404375b8525290210090199bdf9
Milton Araújo <miltonjr.dev@gmail.com>
2022-08-25 15:44:04 -0300

    Improvement: ajust card lifting and created route of contact

---
+++ pages/contato.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const Contact = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1 className="text-center">Fazer formulário</h1>
+    </div>
+  );
+};
+
+export default Contact;
