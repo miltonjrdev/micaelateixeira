e960e26b9913d72e3e273ddd8860ca0d9d5d91d4
Milton Araújo <miltonjr.dev@gmail.com>
2022-08-25 21:33:25 -0300

    feat: contact page

--- pages/contato.js
+++ pages/contato.js
@@ -26,7 +26,7 @@ const Contact5 = () => {
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3005.7959560867184!2d-8.608301084354054!3d41.11714712066677!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd247b8c35b07529%3A0x22be23d93a4b9883!2sStudio%20Micaela%20Teixeira!5e0!3m2!1spt-BR!2sbr!4v1661469126750!5m2!1spt-BR!2sbr"
               title="map"
               scrolling="no"
-              frameborder="0"
+              frameBorder="0"
               width="100%"
               height="300px"
               className=""
